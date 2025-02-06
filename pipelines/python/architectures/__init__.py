835b4d0d0712c959769682a08e2ffb81c722551d
Scott Main <scott@modular.com>
2025-02-05 17:00:59 -0800

    [docs] Add Sphinx config files for pipeline.nn package (#55252)
    Generate docs for the `max.pipelines.nn` package (all modules except
    those in the `hooks` sub-package).
    
    This also adds a new bash script to assist with this kind of task. It
    will generate these .rst files based on a given directory of .py files.
    It takes care of the tedious work but there’s still a little bit of
    manual work to be done (although we could probably upgrade the script to
    do more of it). But we can’t entirely rely on this sort of program; it’s
    just to initialize the files and we must still return to add more
    package/module summaries where appropriate in the RST files.
    
    For this PR, I’m just going for the MVP version of docs. No linting of
    docstrings and no new docstrings.
    
    Fixes DOCS-703
    
    MODULAR_ORIG_COMMIT_REV_ID: 09bedd574517b29dcb8ee0bc8d4d2da08ccc8401

---
+++ pipelines/python/architectures/__init__.py
@@ -0,0 +1,34 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2025, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+from max.pipelines import PIPELINE_REGISTRY
+
+
+def register_all_models():
+    """Imports model architectures, thus registering the architecture in the shared PIPELINE_REGISTRY."""
+    import max.pipelines.llama3 as llama3
+    import max.pipelines.llama_vision as llama_vision
+    import max.pipelines.pixtral as pixtral
+    import max.pipelines.qwen2 as qwen2
+    from max.pipelines.mistral import mistral_arch
+
+    from .mpnet import mpnet_arch
+    from .replit import replit_arch
+
+    architectures = [replit_arch, mistral_arch, mpnet_arch]
+
+    for arch in architectures:
+        PIPELINE_REGISTRY.register(arch)
+
+
+__all__ = ["register_all_models"]
---
+++ pipelines/python/architectures/mpnet/arch.py
@@ -0,0 +1,36 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2025, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+from max.pipelines import (
+    SupportedArchitecture,
+    SupportedEncoding,
+    TextTokenizer,
+    WeightsFormat,
+)
+
+from .model import MPNetPipelineModel
+
+mpnet_arch = SupportedArchitecture(
+    name="MPNetForMaskedLM",
+    example_repo_ids=[
+        "sentence-transformers/all-mpnet-base-v2",
+    ],
+    default_encoding=SupportedEncoding.bfloat16,
+    supported_encodings={
+        SupportedEncoding.float32: [],
+        SupportedEncoding.bfloat16: [],
+    },
+    pipeline_model=MPNetPipelineModel,
+    tokenizer=TextTokenizer,
+    default_weights_format=WeightsFormat.safetensors,
+)
